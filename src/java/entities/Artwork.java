43a18e9e43d38a7601d699c778ae3574707010f5
NazafarinJ <nazafarin.jalilian@mail.utoronto.ca>
2024-11-04 18:14:35 -0500

    made the Artist and museum entities

--- src/java/entities/Artwork.java
+++ src/java/entities/Artwork.java
@@ -1,30 +1,5 @@
 package entities;
 
 public class Artwork {
-    private String title;
-    private String artistName;
-    private String timePeriod;
-    private String genre;
-    private String gallery;
-    private String imageUrl;
-    private String[] keyWords;
 
-    public Artwork(String title, String artistName, String timePeriod, String genre, String gallery,
-                   String imageUrl, String[] keyWords) {
-        this.title = title;
-        this.artistName = artistName;
-        this.timePeriod = timePeriod;
-        this.genre = genre;
-        this.gallery = gallery;
-        this.imageUrl = imageUrl;
-        this.keyWords = keyWords;
-    }
-
-    public String getTitle() { return title; }
-    public String getArtistName() { return artistName; }
-    public String getCompositionDate() { return timePeriod; }
-    public String getGenre() { return genre; }
-    public String getGallery() { return gallery; }
-    public String getImageUrl() { return imageUrl; }
-    public String[] getKeyWords() { return keyWords; }
 }
