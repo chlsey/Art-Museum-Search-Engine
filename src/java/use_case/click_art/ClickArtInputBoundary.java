b8f8b4396869c8f0ddd01b3f8636a4703b1ab9fe
Kathleen <115058370+kaz123456789@users.noreply.github.com>
2024-11-16 15:29:16 -0500

    add click art

--- src/java/use_case/click_art/ClickArtInputBoundary.java
+++
@@ -1,9 +0,0 @@
-package use_case.click_art;
-
-public interface ClickArtInputBoundary {
-    /**
-     * Executes the login use case.
-     * @param clickArtInputData the input data
-     */
-    void execute(ClickArtInputData clickArtInputData);
-}
