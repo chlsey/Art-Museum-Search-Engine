cc9d78dd92f77f882d524899eedf3bcb698623e9
NazafarinJ <nazafarin.jalilian@mail.utoronto.ca>
2024-11-16 16:07:02 -0500

    made the hierarchy of files to get started with writing unit tests!

--- src/java/use_case/rating/RatingOutputData.java
+++ src/java/use_case/rating/RatingOutputData.java
@@ -2,12 +2,18 @@ package use_case.rating;
 
 public class RatingOutputData {
     private int rating;
+    private double averageRating;
 
     public RatingOutputData(int rating, double averageRating) {
         this.rating = rating;
+        this.averageRating = averageRating;
     }
+
     public int getRating() {
         return rating;
     }
 
+    public double getAverageRating() {
+        return averageRating;
+    }
 }
