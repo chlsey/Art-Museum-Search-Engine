ba646b673a9d7015677ca6e723893a7dfc93322c
NazafarinJ <nazafarin.jalilian@mail.utoronto.ca>
2024-11-16 13:54:47 -0500

    The rating use case is complete

--- src/java/use_case/rating/RatingDataAccessInterface.java
+++ src/java/use_case/rating/RatingDataAccessInterface.java
@@ -1,4 +1,8 @@
 package use_case.rating;
-
+import entities.*;
 public interface RatingDataAccessInterface {
+    void getAllRatings();
+    int getRatingCount(int ratingValue);
+    void incrementRatingCount(int ratingValue);
+    double calculateAverageRating();
 }
