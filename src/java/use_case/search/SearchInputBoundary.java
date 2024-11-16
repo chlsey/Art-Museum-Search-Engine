da58423e608625a7c0349b1af12814b95dd66f93
Frappey <xzleo.li@mail.utoronto.ca>
2024-11-16 16:05:51 -0500

    Re-pushed my changes

--- src/java/use_case/search/SearchInputBoundary.java
+++ src/java/use_case/search/SearchInputBoundary.java
@@ -1,10 +1,14 @@
 package use_case.search;
 
+import entities.Artwork;
+
+import java.util.List;
+
 public interface SearchInputBoundary {
 
     /**
      * Executes the search use case.
      * @param searchInputData the input data
      */
-    void execute(SearchInputData searchInputData);
+    List<Artwork> execute(SearchInputData searchInputData);
 }
