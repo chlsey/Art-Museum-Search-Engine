341c32f1a592699c5eb441d5352d0d90f3f620ec
NazafarinJ <nazafarin.jalilian@mail.utoronto.ca>
2024-11-23 14:12:46 -0500

    rating in IA

---
+++ src/java/interface_adapters/rating/RatingState.java
@@ -0,0 +1,9 @@
+package interface_adapters.rating;
+
+public class RatingState {
+    private int rating;
+
+    public RatingState() {}
+    public void setRating(int rating) {this.rating = rating;}
+    public int getRating() {return this.rating;}
+}
