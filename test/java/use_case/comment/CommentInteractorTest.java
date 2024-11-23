1e6e65f9a2c99f249d1624d8b150d0c598300c6f
NazafarinJ <nazafarin.jalilian@mail.utoronto.ca>
2024-11-23 15:20:32 -0500

    test for comment

--- test/java/use_case/comment/CommentInteractorTest.java
+++ test/java/use_case/comment/CommentInteractorTest.java
@@ -1,11 +1,20 @@
 package use_case.comment;
-import static org.junit.Assert.*;
+import entities.*;
+import org.junit.Before;
 import org.junit.Test;
 import data.InMemoryDataAccessObject;
 import org.junit.jupiter.api.BeforeEach;
-import entities.*;
 
+import static org.junit.Assert.assertEquals;
 
 
 public class CommentInteractorTest {
+    @BeforeEach
+    void beforeEach() {
+
+    }
+    @Test
+    public void commentInteractorTest() {
+        CommentInputData commentInputData = new CommentInputData("rain", "cool");
+    }
 }
