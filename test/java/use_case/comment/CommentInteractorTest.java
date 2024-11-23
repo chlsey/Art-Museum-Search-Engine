7cffd4b7482df8f339ed9370f7982266e5d8da70
NazafarinJ <nazafarin.jalilian@mail.utoronto.ca>
2024-11-23 15:15:17 -0500

    test for comment

---
+++ test/java/use_case/comment/CommentInteractorTest.java
@@ -0,0 +1,11 @@
+package use_case.comment;
+import static org.junit.Assert.*;
+import org.junit.Test;
+import data.InMemoryDataAccessObject;
+import org.junit.jupiter.api.BeforeEach;
+import entities.*;
+
+
+
+public class CommentInteractorTest {
+}
