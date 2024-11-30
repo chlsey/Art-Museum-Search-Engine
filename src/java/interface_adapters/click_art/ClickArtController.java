4a5de5aa41d1434bb75faad189aa978443ef3f61
Kathleen <115058370+kaz123456789@users.noreply.github.com>
2024-11-29 23:49:39 -0500

    search view

---
+++ src/java/interface_adapters/click_art/ClickArtController.java
@@ -0,0 +1,27 @@
+package interface_adapters.click_art;
+
+import use_case.click_art.ClickArtInputBoundary;
+import use_case.click_art.ClickArtInputData;
+import entities.Artwork;
+
+public class ClickArtController {
+    private final ClickArtInputBoundary clickArtUseCaseInteractor;
+
+    public ClickArtController(ClickArtInputBoundary ClickArtUseCaseInteractor) {
+        this.clickArtUseCaseInteractor = ClickArtUseCaseInteractor;
+    }
+
+    public void execute(Artwork artwork) {
+        final ClickArtInputData clickArtInputData = new ClickArtInputData(artwork);
+        clickArtUseCaseInteractor.execute(clickArtInputData);
+    }
+
+    public void switchToSearch(){
+        clickArtUseCaseInteractor.switchToSearchView();
+    }
+
+    public void switchToCFR(Artwork artwork){
+        clickArtUseCaseInteractor.switchToCFRView(new ClickArtInputData(artwork));
+    }
+}
+
