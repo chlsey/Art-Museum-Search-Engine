08ee9e2515e1add195256da0f91a25e61b40ef86
JYW5114 <elowenyiwei.jiang@mail.utoronto.ca>
2024-12-02 01:45:47 -0500

    Code review of comment use case.

--- src/main/java/use_case/comment/CommentDataAccessInterface.java
+++ src/main/java/use_case/comment/CommentDataAccessInterface.java
@@ -1,12 +1,33 @@
 package use_case.comment;
 
-import entities.Artwork;
 import java.io.IOException;
 import java.util.List;
 
+import entities.Artwork;
+
+/**
+ * Comment Data Access Interface.
+ */
 public interface CommentDataAccessInterface {
+    /**
+     * Add a comment.
+     * @param artwork Artwork
+     * @param comment String
+     * @throws IOException String
+     */
     void addCommentToArtwork(Artwork artwork, String comment) throws IOException;
+
+    /**
+     * Get artwork by its id.
+     * @param id String
+     * @return the artwork with the same id.
+     * @throws IOException string
+     */
     Artwork getArtworkById(String id) throws IOException;
 
+    /**
+     * Get all commented artworks.
+     * @return a list of commented artworks.
+     */
     List<Artwork> getCommentedArtworks();
 }
