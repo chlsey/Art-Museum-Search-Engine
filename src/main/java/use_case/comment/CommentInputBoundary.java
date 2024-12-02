1dc6564761be49fb41907b0c42561e15a54a2148
JYW5114 <elowenyiwei.jiang@mail.utoronto.ca>
2024-12-02 01:48:03 -0500

    Code review of comment use case.

--- src/main/java/use_case/comment/CommentInputBoundary.java
+++ src/main/java/use_case/comment/CommentInputBoundary.java
@@ -1,12 +1,24 @@
 package use_case.comment;
 
-import entities.Artwork;
-
 import java.io.IOException;
 import java.util.List;
 
+import entities.Artwork;
+
+/**
+ * Comment Inpuut Boundary.
+ */
 public interface CommentInputBoundary {
+    /**
+     * Add comment.
+     * @param commentInputData CommentInputData
+     * @throws IOException failed comment
+     */
     void addComment(CommentInputData commentInputData) throws IOException;
 
+    /**
+     * Get all commented artworks.
+     * @return a list of artworks
+     */
     List<Artwork> getCommentedArtworks();
 }
